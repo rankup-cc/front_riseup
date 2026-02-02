22d2ad8b637bb66df059b7766f29a1e3f1a64b07
rankup-cc <ctcougnaud2@gmail.com>
2026-02-02 19:23:19 +0100

    Simplify login email placeholder

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -98,7 +98,7 @@ export default function Login() {
                                         <FormItem>
                                             <FormLabel>Email</FormLabel>
                                             <FormControl>
-                                                <Input type="email" autoComplete="email" placeholder="jimmy.gressier@kiprun.com" {...field} />
+                                                <Input type="email" autoComplete="email" placeholder="exemple@mail.com" {...field} />
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
